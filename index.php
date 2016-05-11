f8d1873da283f3250de8498bf73aac519813db18
caokailiang <350271335@qq.com>
2016-05-11 17:11:27 +0800

    try test

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$array = array(); 
+$array['key'] = 'website'; 
+$array['value']='www.chhua.com'; 
+$a = serialize($array); 
+echo $a; 
+unset($array); 
+$a = unserialize($a); 
+print_r($a); 
+
+phpinfo();
